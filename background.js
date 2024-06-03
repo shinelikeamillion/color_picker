a7e1a7804d3ee014b07c2f3cb7c907f0bc53c8eb
Sean Liu <geeks.liu@gmail.com>
2024-06-03 23:49:28 +1000

    better structure

--- background.js
+++ background.js
@@ -1,17 +1,7 @@
-// Listen for a click on the camera icon. On that click, take a screenshot.
+
 chrome.action.onClicked.addListener(function () {
   capture();
 });
-
-// chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-//   if (message.type === "position") {
-//     // const screenshotUrl = await chrome.tabs.captureVisibleTab();
-//     sendResponse({
-//       // url: screenshotUrl,
-//     });
-//     s;
-//   }
-// });
 
 function capture() {
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
--- content.js
+++ content.js
@@ -6,15 +6,83 @@ let scale = 4;
 let active = false;
 
 document.addEventListener("mousemove", async (e) => {
-  position = [e.clientX, e.clientY];
+  if (!active) return;
   canvas = document.getElementById("picker");
 
-  const x = position[0];
-  const y = position[1];
   if (!tempCanvas) return;
+  drawTempCanvas(e.clientX, e.clientY)
+  drawPixels()
+});
+
+let tempCanvas;
+let image;
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // only response the color picker message
+  if (message.type !== "color_picker") return;
+
+  // when there's container already, remove it at first
+  if (document.getElementById("container")) {
+    document.getElementById("container").remove();
+    active = false;
+    return;
+  }
+
+  active = true;
+  createCanvas(message);
+});
+
+function createCanvas(message) {
+  image = new Image();
+  image.src = message.image;
+  image.onload = () => {
+    wrapper = document.createElement("div");
+    wrapper.setAttribute("id", "wrapper");
+    const container = document.createElement("div");
+    container.setAttribute("id", "container");
+    document.body.appendChild(container);
+    container.appendChild(wrapper);
+    container.addEventListener("click", (e) => {
+      active = !active;
+      color = document.getElementById("wrapper_p").innerText;
+      navigator.clipboard.writeText(color);
+      const msg = document.createElement("p");
+      msg.innerText = "color copied.";
+      wrapper.appendChild(msg);
+      setTimeout(() => {
+        msg.remove();
+        container.remove();
+      }, 1000);
+    });
+
+    // create tempcanvas
+    tempCanvas = document.createElement("canvas");
+    tempCanvas.width = size;
+    tempCanvas.height = size;
+    tempCanvas.style.borderRadius = `${size / 2}px`;
+    tempCanvas.setAttribute("id", "temp");
+
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "picker");
+    canvas.width = size;
+    canvas.height = size;
+    canvas.style.borderRadius = `${size / 2}px`;
+    wrapper.appendChild(canvas);
+
+    text = document.createElement("p");
+    text.setAttribute("id", "wrapper_p");
+    text.innerText = "#fffff";
+    wrapper.appendChild(text);
+  };
+}
+
+function drawTempCanvas(x, y){
   const tempSize = size / scale;
   const centerTemp = tempSize / 2;
-  const tempCtx = tempCanvas.getContext("2d", {alpha: false, willReadFrequently: true});
+  const tempCtx = tempCanvas.getContext("2d", {
+    alpha: false,
+    willReadFrequently: true,
+  });
   // Setup the pixelation of the source image on the canvas.
   tempCtx.drawImage(
     image,
@@ -27,62 +95,27 @@ document.addEventListener("mousemove", async (e) => {
     size,
     size
   );
+}
 
-  const ctx = canvas.getContext("2d", {alpha: false, willReadFrequently: true});
+function drawPixels() {
+  const ctx = canvas.getContext("2d", {
+    alpha: false,
+    willReadFrequently: true,
+  });
+  const tempCtx = tempCanvas.getContext("2d", {
+    alpha: false,
+    willReadFrequently: true,
+  });
   ctx.mozImageSmoothingEnabled = false;
   ctx.webkitImageSmoothingEnabled = false;
   ctx.imageSmoothingEnabled = false;
-
-  // second way
-  // // const small = 17;
-  // const ctx = canvas.getContext("2d", {alpha: false, willReadFrequently: true});
-  // ctx.mozImageSmoothingEnabled = false;
-  // ctx.webkitImageSmoothingEnabled = false;
-  // ctx.imageSmoothingEnabled = false;
-
-  // ctx.drawImage(
-  //   image,
-  //   x - centerTemp,
-  //   y - centerTemp,
-  //   tempSize,
-  //   tempSize,
-  //   0,
-  //   0,
-  //   small,
-  //   small
-  // );
-  // ctx.rect(0, 0, size, size);
-  // ctx.drawImage(canvas, 0, 0, small, small, 0, 0, size, size);
-
-  // let mi = parseInt(170 / 2);
-  // const color = ctx.getImageData(mi, mi, 1, 1).data;
-  // seletedColor = rgbToHex(color[0], color[1], color[2]);
-  // document.getElementById("wrapper_p").innerText = seletedColor;
-
-  // const s = size / small;
-  // for (i = 0; i < small; i++) {
-  //   ctx.strokeStyle = "#000";
-  //   ctx.lineWidth = 0.2;
-  //   ctx.beginPath();
-  //   ctx.moveTo(0, i * s + 0.5);
-  //   ctx.lineTo(size, s * i + 0.5);
-  //   ctx.moveTo(i * s + 0.5, 0);
-  //   ctx.lineTo(s * i + 0.5, size);
-  //   ctx.stroke();
-  // }
-  // let m = parseInt(17 / 2);
-  // ctx.lineWidth = 1;
-  // ctx.strokeStyle = "#000";
-  // ctx.strokeRect(s * m + 0.5, s * m + 0.5, s, s);
-  // ctx.strokeStyle = "#fff";
-  // ctx.strokeRect(s * m - 0.5, s * m - 0.5, s + 1.5, s + 1.5);
 
   let seletedColor = "";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (i = 0; i < num; i++) {
     for (j = 0; j < num; j++) {
-      const cx = map(i, 0, num, 0, size) + 5
-      const cy = map(j, 0, num, 0, size) + 5
+      const cx = map(i, 0, num, 0, size) + 5;
+      const cy = map(j, 0, num, 0, size) + 5;
       const color = tempCtx.getImageData(cx, cy, 1, 1).data;
       let rbgColor = `rgb(${color[0]} ${color[1]} ${color[2]})`;
       ctx.fillStyle = rbgColor;
@@ -108,42 +141,7 @@ document.addEventListener("mousemove", async (e) => {
   ctx.strokeRect(10 * center + 0.5, 10 * center + 0.5, 10, 10);
   ctx.strokeStyle = "#fff";
   ctx.strokeRect(10 * center - 0.5, 10 * center - 0.5, 11.5, 11.5);
-});
-let tempCanvas;
-let image;
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if(message.type !== 'color_picker') return;
-  image = new Image();
-  image.src = message.image;
-  image.onload = () => {
-    wrapper = document.createElement("div");
-    wrapper.setAttribute("id", "wrapper");
-    document.body.appendChild(wrapper);
-
-    tempCanvas = document.createElement("canvas");
-    tempCanvas.width = size;
-    tempCanvas.height = size;
-    tempCanvas.style.borderRadius = `${size / 2}px`;
-    tempCanvas.setAttribute("id", "temp");
-
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute("id", "picker");
-    canvas.width = size;
-    canvas.height = size;
-    canvas.style.borderRadius = `${size / 2}px`;
-    wrapper.appendChild(canvas);
-    // wrapper.appendChild(tempCanvas);
-    // img = document.createElement("img");
-    // img.src = message.image;
-    // document.body.appendChild(img);
-
-    text = document.createElement("p");
-    text.setAttribute("id", "wrapper_p");
-    text.innerText = "#fffff";
-    wrapper.appendChild(text);
-  };
-});
+}
 
 function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
